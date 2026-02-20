8cdefeb988901b9c49ac0ea1d33c24a80d325524
xoliqberdiyev <xoliqberdiyevbehruz12@gmail.com>
2026-02-20 16:06:24 +0500

    fix

--- resources/scripts/entrypoint.sh
+++ resources/scripts/entrypoint.sh
@@ -2,6 +2,6 @@
 python3 manage.py collectstatic --noinput
 python3 manage.py migrate --noinput
 
-uvicorn config.asgi:application --host 0.0.0.0 --port 8000 --reload --reload-dir core --reload-dir common
+uvicorn core.asgi:application --host 0.0.0.0 --port 8000 --reload --reload-dir core --reload-dir common
 
 exit $?
